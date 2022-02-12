7a4f430f2ba524cd779598bc71fd835a37be3eae
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-02-11 22:13:09 -0300

    forms fb

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -6,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
 
-  constructor() { }
+  public exampleControl: FormControl;
+  public exampreGroup: FormGroup;
+  public exampleArray: FormArray;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.exampleControl = fb.control('');
+
+    this.exampreGroup = fb.group({
+      email: '',
+      password: ''
+    });
+
+    this.exampleArray = fb.array(
+      [
+        {
+          pregunta: '123'
+        },
+        {
+          tipo: '456'
+        }
+      ]
+    );
+
+    // this.example = _fb.control({value: 'my val', disabled: true});
   }
+  ngOnInit(): void {
+    // console.log(this.exampleControl);
+    this.exampleArray.controls.forEach(element => console.log(element)); // listado de los objetos del array
+    console.log(this.exampleArray.controls.length); // conteo
+    console.log(this.exampleArray.controls.map(element => element.value)); // listado de los valores del array
+  }
+
+
+
 
 }
